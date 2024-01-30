5f33f692951ab5efe076e152979984e3a1a24d9c
Zrybea Whitfield <66852175+ZrybeaWhitfield@users.noreply.github.com>
2024-01-30 18:37:56 -0500

    Refactored up to exercise 1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,20 +1,52 @@
-const Hello = () => {
+const Header = (props) => {
+    return (
+      <h1>{props.course}</h1>
+    )
+}
+
+const Content = (props) => {
+  const p = props.parts.map(prop => prop.name + " " + prop.exercises)
+  
   return (
-    <div>
-      <p>Hello World 👾</p>
-    </div>
+    [p[0],<br></br>, p[1],<br></br>, p[2]]
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.total}</p>
   )
 }
 
 const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  const total = (parts) => {
+    let sum = 0;
+    parts.forEach(p => sum += p.exercises)
+    return sum
+  }
 
   return (
-    <div>
-      <h1>Greetings Earthlings!</h1>
-      <Hello />
-      <Hello />
-      <Hello />
-    </div>
+    <>
+      <Header course={course} />
+      <Content parts={parts}/>
+      <Total total={total(parts)}/>
+    </>
   )
 }
 
