b48325fc849aab1353fcbfb1b20688e989cd7d6c
Zrybea Whitfield <66852175+ZrybeaWhitfield@users.noreply.github.com>
2024-01-29 23:24:06 -0500

    Established React app using vite

---
+++ part1/src/App.jsx
@@ -0,0 +1,21 @@
+const Hello = () => {
+  return (
+    <div>
+      <p>Hello World 👾</p>
+    </div>
+  )
+}
+
+const App = () => {
+
+  return (
+    <div>
+      <h1>Greetings Earthlings!</h1>
+      <Hello />
+      <Hello />
+      <Hello />
+    </div>
+  )
+}
+
+export default App
